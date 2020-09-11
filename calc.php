4729e109e0380e7bc758565da7ba5390d427edd8
Renato Ribeiro <69020753+iSlow1@users.noreply.github.com>
2020-09-11 10:43:36 -0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,34 +1,15 @@
 <?php
 
-echo 'Digite dois numero, para saber os resultados de suas operações';
-
 $primeiroNumero = $_GET['primeiroNumero'];
 $segundoNumero = $_GET['segundoNumero'];
-$op = $_GET['op'];
-switch ($_GET['op'])
-{
-    case '+'
-        $resultado = $primeiroNumero + $segundoNumero;
-        echo "A soma e $result";
-    break;
-    case '-'
-        $resultado = $primeiroNumero - $segundoNumero;
-        echo "A subtracao e $result";
-    break;  
-    case '*'
-        $resultado = $primeiroNumero * $segundoNumero;
-        echo "A multiplicacao e $result";
-    break; 
-    case '/'
-        if ($primeiroNumero = 0 || $segundoNumero = 0)
-        {
-            echo 'Impossivel realizar divisao por 0'
-        }
-        else
-        {
-            $resultado = $primeiroNumero / $segundoNumero;
-            echo "A divisao e $result";
-            break; 
-        }
 
-}
+$soma = $primeiroNumero + $segundoNumero;
+$subtracao = $primeiroNumero - $segundoNumero;
+$multiplicacao = $primeiroNumero * $segundoNumero;
+$divisao = $primeiroNumero / $segundoNumero;
+
+
+echo "A Soma é: $soma <br>";
+echo "A Subtração é: $subtracao <br>";
+echo "A Multiplicação é: $multiplicacao <br>";
+echo "A Divisão é: $divisao <br>";
